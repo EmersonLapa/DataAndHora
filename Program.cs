fb8629835da461dc7c6a9557a678fbee91b33f62
Emerson <emersonlapa@outlook.com>
2020-10-26 09:23:27 -0300

    Upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace DataEHora
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá Digite uma Data: ");
+            DateTime d1 = DateTime.Parse(Console.ReadLine());
+            Console.WriteLine("Data Local: " + d1);
+            Console.WriteLine("Data UTC: " + d1.ToUniversalTime());
+            Console.WriteLine("Apenas a hora: " + d1.ToString("HH:mm:ss"));
+        }
+    }
+}
